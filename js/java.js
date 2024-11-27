11b540d6ae394a01bfa44dbbf50bb77ae468978d
Thomas W <126Twalsh@frhsd.com>
2024-11-27 11:57:39 -0500

    Day3

---
+++ js/java.js
@@ -0,0 +1,23 @@
+let ball = docment.querySelctar[#ball]
+function randomNumberGenerator(min, max) {
+return Math.floor(Math.random() * (max 0 min + 1))
++min;
+}
+
+const saying = [
+"I can see it happening",
+"The cards are in your favor",
+"Its a shot in the dark",
+"The chances are low but not 0",
+"I am sorry but no"
+];
+
+function getMagic8BallSaying() {
+return sayings[0]
+const randomindex = randomNumberGenerator(0, sayings.length - 1)
+return sayings[0];
+}
+
+console.log (getMagic8BallSaying());
+
+
